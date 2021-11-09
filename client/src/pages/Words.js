04b4a2c3ef41e3f02c99aa3b88b3f49290e9c5cb
Vicky Ungemach <vicky.ungemach@gmail.com>
2021-11-09 05:53:10 -0800

    delete a word

--- client/src/pages/Words.js
+++ client/src/pages/Words.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 import { connect } from 'react-redux'
 import { getLists, displayList } from 'actions/lists'
-import { getWords, openEdit, openCreate } from 'actions/words'
+import { getWords, openEdit, openCreate, deleteWord } from 'actions/words'
 import { openSlide } from 'actions/utils';
 import wordlistSelector from 'selectors/wordlistSelector'
 
@@ -21,7 +21,7 @@ import WordsPanel from 'components/blocks/words/WordsPanel';
 
 
 
-const Words = ({ displayList, match, wordList, getWords, getLists, openEdit, openCreate, openSlide }) => {
+const Words = ({ displayList, match, wordList, getWords, getLists, openEdit, openCreate, openSlide, deleteWord }) => {
     const [hidden, setHidden] = useState(true);    // Half screen form
 
     hideByClick(setHidden, 'closing-x');
@@ -47,6 +47,14 @@ const Words = ({ displayList, match, wordList, getWords, getLists, openEdit, ope
         openEdit(word._id, word.foreign, word.native);
     }
 
+    const onDelete = (id) => {
+        const c = window.confirm("Delete this word?");
+        
+        if(c) {
+            deleteWord(id);
+        }
+    }
+
     return (
         <Container>
             <ScrollContainer>
@@ -64,7 +72,12 @@ const Words = ({ displayList, match, wordList, getWords, getLists, openEdit, ope
                     <ListContainer flex>
                         {
                             wordList.map(word => (
-                                <WordItem key={word._id} word={word} onEdit={() => onEdit(word)} />
+                                <WordItem 
+                                    key={word._id} 
+                                    word={word} 
+                                    onEdit={() => onEdit(word)} 
+                                    onDelete={() => onDelete(word._id)} 
+                                />
                             ))
                         }
                     </ListContainer>
@@ -84,5 +97,5 @@ const mapStateToProps = state => ({
     wordList: wordlistSelector(state)
 })
 
-export default connect(mapStateToProps, { displayList, getLists, getWords, openEdit, openCreate, openSlide })(Words);
+export default connect(mapStateToProps, { displayList, getLists, getWords, openEdit, openCreate, openSlide, deleteWord })(Words);
 
