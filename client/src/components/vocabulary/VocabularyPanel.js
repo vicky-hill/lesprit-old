4c5883b877b0fcd53f2dec948d5c3d3236221e58
Vicky Ungemach <vicky.ungemach@gmail.com>
2021-11-11 07:17:21 -0800

    create outline button style

--- client/src/components/vocabulary/VocabularyPanel.js
+++ client/src/components/vocabulary/VocabularyPanel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Searchbar from 'components/elements/Searchbar';
 import Card from 'components/elements/Card';
 import Button from 'components/elements/Button';
+import { Plus } from 'react-feather';
 
 /* Props
 =================================================== */
@@ -15,7 +16,7 @@ function VocabularyPanel({ openForm, count }) {
         <Card type="panel" radius="medium">
             <div className="panel-card_group">
                 <h1 className="panel-card_group--title">{ count } Words</h1>
-                <Button id="add-list-btn" type="transparent" onClick={openForm}><p>+</p> Add new list</Button>
+                <Button id="add-list-btn" type="transparent" onClick={openForm}><Plus color="#eebf3d" size={14} /><span>Add List</span></Button>
             </div>
             <Searchbar />
         </Card>
--- client/src/components/words/WordsPanel.js
+++ client/src/components/words/WordsPanel.js
@@ -4,6 +4,7 @@ import Card from 'components/elements/Card';
 import InputText from 'components/elements/InputText';
 import { connect } from 'react-redux'
 import { updateList } from 'actions/lists';
+import { Plus, Trash } from 'react-feather';
 
 
 /* Props
@@ -15,7 +16,7 @@ function WordsPanel({ openForm, activeList, loading, history, updateList }) {
     const onSubmit = (title) => {
         updateList(activeList._id, { title })
     }
-     
+
     return (
         loading ? <p>loading</p> : (
             <Card type="panel" radius="medium" >
@@ -25,7 +26,11 @@ function WordsPanel({ openForm, activeList, loading, history, updateList }) {
                         <p className="panel-card_group--subtitle">Words</p>
                     </div>
                 </div>
-                <Button type="transparent" onClick={openForm}><p>+</p> Add new Word</Button>
+                <div className="panel-card_group higher">
+                    <Button type="outline" onClick={openForm}><Plus color="#514F55" size={12} /> <span>Add Words &nbsp; </span></Button>
+                    <Button type="outline" onClick={openForm}><Trash color="#514F55" size={15} /></Button>
+                </div>
+
             </Card >
         )
     )
