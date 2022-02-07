ae9c6c258224933aa6e385bd28b56cf97569d484
Vicky Ungemach <vicky.ungemach@gmail.com>
2022-02-07 14:01:43 -0800

    populate overview table

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getWords } from 'actions/words';
 
-function Overview() {
+
+function Overview({ words, loading, getWords }) {
+    useEffect(() => {
+        getWords();
+    }, [])
+
+    function getTime(date) {
+        const countDate = new Date(date);
+        const now = new Date().getTime();
+        const gap = countDate - now;
+      
+        if(gap <= 0) {
+          return "--";
+        }
+      
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
+      
+          const d = Math.floor(gap / (day));
+          const h = Math.floor(gap % (day) / hour);
+          const m = Math.floor(gap % (hour) / minute);
+      
+      
+          const result = `${d}d, ${h}h, ${m}m`;
+      
+          return result;
+      }
+
     return (
         <div className='table-container'>
             <table className='table sticky'>
@@ -13,16 +44,25 @@ function Overview() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><strong>loco</strong></td>
-                        <td>crazy</td>
-                        <td>5</td>
-                        <td>2h</td>
-                    </tr>
+                    {
+                        !loading && words.map(word => (
+                            <tr>
+                                <td><strong>{word.foreign}</strong></td>
+                                <td>{word.native}</td>
+                                <td>{word.rating}</td>
+                                <td>{getTime(word.dueDate)}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
     );
 }
 
-export default Overview;
+const mapStateToProps = state => ({
+    words: state.words.words,
+    loading: state.words.loading
+})
+
+export default connect(mapStateToProps, { getWords })(Overview);
